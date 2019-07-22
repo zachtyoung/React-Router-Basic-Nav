6e72f9eea0406c5c576982630e23426f96e068af
zachtyoung <zachyoung@Zachs-MacBook-Pro.local>
2019-07-22 17:00:12 -0500

    Sanity Commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import { Home, About, Contact, Navigation } from './components';
 
+
 const App = () => (
+  <Router>
   <div>
     <Navigation />
+    <Switch>
+      <Route exact path='/' component={Home} />
+      <Route exact path='/about' component={About} />
+      <Route exact path='/contact' component={Contact} />
+    </Switch>
   </div>
+  </Router>
 );
 
 export default App;
